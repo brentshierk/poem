ab64305353debb2f75ba4a6e6d8a1f583b4e5395
Brent_Shierk <bshierk1996@gmail.com>
2020-10-02 01:18:11 -0400

    compleated hw

---
+++ app.py
@@ -0,0 +1,58 @@
+import random
+#open file
+
+
+def get_file_lines(filename):
+    filename = open('poem.txt', 'r')
+    line_list = filename.readlines()
+    #print(line_list)
+    return line_list
+
+    #lines_list = []
+    # for line in filename:
+    #     newline = line.strip()
+    #     split_list_of_lines = newline.split()
+    #     split_list_of_lines
+    #     print(split_list_of_lines)
+    #     #return split_list_of_lines
+       
+        
+line_list = get_file_lines('poem.txt')
+
+
+# def lines_printed_backwards(line_list):
+    
+#     #print(reveresed_lines)
+#     list_num = list(range(len(line_list)))
+
+#     for i in list(reversed(list_num)):
+#         print(f'{i + 1} {line_list[i]}')
+
+# lines_printed_backwards(line_list)
+
+def lines_printed_random(line_list):
+    #print(line_list)
+    #random_line = range(len(line_list))
+    i = 0
+    while i < len(line_list):
+        random_line = random.randrange(100)
+        print(line_list[random_line])
+        i += 1 
+    
+lines_printed_random(line_list)
+
+def lines_printed_custom(lines_list):
+    
+    for line in lines_list:
+        string = ''
+        num_list = range(len(line))
+        num_list = list(num_list)
+        for i in num_list:
+            if line[i] != ' ' and i % 2 == 0:
+                string += line[i].upper()
+            else:
+                string += line[i]
+        print(string)
+        
+lines_printed_custom(line_list)
+    
